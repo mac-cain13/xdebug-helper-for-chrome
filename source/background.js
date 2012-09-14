8ea96f0d889161c2409a718536bd8830a0d6ca66
Mathijs Kadijk <mathijs@wrep.nl>
2012-09-14 16:05:22 +0200

    Initial GitHub version
    - Added Readme
    - Added License
    - Removed screenshots/keys/etc

---
+++ source/background.js
@@ -0,0 +1,104 @@
+chrome.tabs.onUpdated.addListener(function(tabid)
+{
+	chrome.tabs.getSelected(null, function(tab)
+	{
+		if (!localStorage || !localStorage['sites']) {
+			return;
+		}
+		sites = localStorage["sites"];
+		
+		sites = JSON.parse(sites);
+
+		baseDomain = tab.url.match(/:\/\/(.[^/]+)/)[1];
+
+		match = isValueInArray(sites, baseDomain);
+		
+		if (match || sites.length == 0)
+		{
+			chrome.pageAction.show(tabid);
+			chrome.tabs.getSelected(null, function(tab)
+			{
+				chrome.tabs.sendRequest(
+					tab.id,
+					{
+						cmd: "status", idekey: localStorage["xdebugIdeKey"]
+					},
+					function(response) 
+					{
+						updateIcon(response.result, tabid);
+					}
+				);
+			});
+		}
+	});
+});
+
+chrome.pageAction.onClicked.addListener(function(tab)
+{
+	chrome.tabs.getSelected(null, function(tab)
+	{
+		chrome.tabs.sendRequest(
+			tab.id,
+			{
+				cmd: "toggle", idekey: localStorage["xdebugIdeKey"]
+			},
+			function(response){
+				updateIcon(response.result, tab.id);
+			}
+		);
+	});
+});
+
+function updateIcon(status, tabid)
+{
+	if (status == 1) 
+	{
+		chrome.pageAction.setTitle({
+			tabId: tabid,
+			title: "Debugging enabled"
+		});
+
+		chrome.pageAction.setIcon({
+			tabId: tabid,
+			 path: "images/bug.png"
+		});
+	} 
+	else if (status == 2) 
+	{
+		chrome.pageAction.setTitle({
+			tabId: tabid,
+			title: "Profiling enabled"
+		});
+
+		chrome.pageAction.setIcon({
+			tabId: tabid,
+			 path: "images/clock.png"
+		});
+	} 
+	else 
+	{
+		chrome.pageAction.setTitle({
+			tabId: tabid,
+			title: "Debugging & profiling disabled"
+		});
+
+		chrome.pageAction.setIcon({
+		   tabId: tabid,
+			path: "images/bug-gray.png"
+		});
+	}
+}
+
+function isValueInArray(arr, val) 
+{
+	for (i = 0; i < arr.length; i++)
+	{
+		re = new RegExp(arr[i], "gi");
+		if (re.test(val))
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
---
+++ source/options.js
@@ -0,0 +1,107 @@
+
+function save_options() 
+{
+	input = document.getElementById("idekey");
+	idekey = input.value;
+	localStorage["xdebugIdeKey"] = idekey;
+
+	siteBox = document.getElementById("siteBox");
+	sites = [];
+	for (i = 0; i < siteBox.length; i++)
+	{
+		sites.push(siteBox.options[i].value);
+	}
+	localStorage["sites"] = JSON.stringify(sites);
+}
+
+function restore_options()
+{
+	idekey = localStorage["xdebugIdeKey"];
+
+	if (!idekey)
+	{
+		idekey = "XDEBUG_ECLIPSE";
+	}
+
+	if (idekey == "XDEBUG_ECLIPSE" || idekey == "netbeans-xdebug")
+	{
+		$('#ide').val(idekey);
+	}
+	else
+	{
+		$('#ide').val("null");
+		$("#customkey").fadeIn();
+	}
+	$('#idekey').val(idekey);
+
+	sites = localStorage["sites"];
+	sites = JSON.parse(sites);
+	for(i = 0; i < sites.length; i++)
+	{
+		addItem("siteBox", sites[i]);
+	}
+}
+
+function addSite() 
+{
+	siteText = document.getElementById("newSite").value;
+	addItem("siteBox", siteText);
+	save_options();
+	document.getElementById("newSite").value = '';
+	
+	save_options();
+}
+
+function removeSelectedSite() 
+{
+	siteBox = document.getElementById("siteBox");
+	for (i = siteBox.length-1; i >= 0; i--) {
+		if (siteBox.options[i].selected)
+		{
+			siteBox.remove(i);
+		}
+	}
+
+	save_options();
+}
+
+function addItem(id, value)
+{
+	opt = document.createElement("option");
+	document.getElementById(id).options.add(opt);
+	opt.value = value;
+	opt.text = value;
+}
+
+$(document).ready(function() 
+{
+	$("#ide").change(function () 
+	{
+		if ($("#ide").val() != "null") 
+		{
+			$("#customkey").fadeOut();
+			$("#idekey").val($("#ide").val());
+			
+			 save_options();
+		} 
+		else 
+		{
+			$("#customkey").fadeIn();
+		}
+	});
+	
+	$("#idekey").change(function() 
+	{
+		save_options();
+	});
+	
+	$('#add-site').click(function(){
+		addSite();
+	});
+	
+	$('#remove-site').click(function(){
+		removeSelectedSite();
+	});
+	
+	restore_options();
+});
