d85777b025df99dde2edd9fb0c62b47d9827eb41
Mathijs Kadijk <mathijs@wrep.nl>
2013-01-27 15:21:18 +0100

    Popup switches to the correct state

--- source/popup.js
+++ source/popup.js
@@ -1,11 +1,14 @@
 $(function() {
 	$("a").on("click", function(eventObject) {
+		var requestedStatus = $(this).data("action");
+
 		chrome.tabs.getSelected(null, function(tab)
 		{
 			chrome.tabs.sendRequest(
 				tab.id,
 				{
-					cmd: "toggleStatus",
+					cmd: "setStatus",
+					status: requestedStatus,
 					idekey: localStorage["xdebugIdeKey"]
 				},
 				function(response)
